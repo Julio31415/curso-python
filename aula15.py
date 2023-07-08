b42f5b45e9516e3599a5d2152ce3a19d416ac528
JULIO <juliocfs31415@gmail.com>
2023-07-08 19:40:02 -0300

    adicionando aulas

---
+++ aula15.py
@@ -0,0 +1,7 @@
+#nome = input ("Qual o seu nome ? ")
+#print(f'O seu nome é {nome}')
+
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro numero: '))
+
+print(f'A soma dos numero é {numero_1 + numero_2}')
---
+++ aula17.py
@@ -0,0 +1,13 @@
+#if / elif /else
+
+condicao = True
+
+if condicao:
+    print('Este é o código do if')
+else:
+    print('')
+
+if 10 == 10:
+    print('Outro if')
+
+print('fora do if')

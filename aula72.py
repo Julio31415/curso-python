04b6d566e3664992e185b257406bdc23af454e2f
Júlio <juliocfs31415@gmail.com>
2023-10-29 13:43:43 -0300

    subindo conclusão do exercicio da aula 72

--- aula72.py
+++ aula72.py
@@ -11,4 +11,25 @@ perguntas = [
      'opcoes': ['15','5','25','19'],
      'resposta': "5"}
 ]
+contador = 0
+contador_opcoes = 0
+contador_acertos = 0
+print("SISTEMA DE PERGUNTAS E RESPOSTAS:")
+while contador<len(perguntas):
+    print(perguntas[contador].get('pergunta'))
+    while contador_opcoes<len(perguntas[contador].get('opcoes')):
+        resposta = perguntas[contador].get('opcoes')[contador_opcoes]
+        indice = contador_opcoes + 1
+        print(f'{indice} - {resposta}')
+        print(" ")
+        contador_opcoes = contador_opcoes + 1
+    retorno = str(input("Qual o valor da opção correta?"))
+    if retorno == perguntas[contador].get('resposta'):
+        print("Parabéns, seu animal, você acertou")
+        contador_acertos = contador_acertos + 1
+    else:
+        print("Errou!")
+    contador = contador + 1
+    contador_opcoes = 0
+print(f'Você acertou {contador_acertos}/3. Parabens')
 
